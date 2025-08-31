3cef7e3483698cd17df04323fe3d9c96feaf212a
P-KAVYASRI <kavyasri369369@gmail.com>
2025-08-31 23:51:05 +0530

    newcommit

--- server/server.js
+++ server/server.js
@@ -9,3 +9,7 @@ const app= express()
  app.get('/',(req,res)=>res.send('Server is Live!'))
 
  const PORT = process.env.PORT || 3000
+
+ app.listen(PORT, ()=>{
+    console.log('Server is running on port', PORT);
+ })
