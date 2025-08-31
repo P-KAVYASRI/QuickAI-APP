bced412bc814065ff5413e92c580cd8ca407eb46
P-KAVYASRI <kavyasri369369@gmail.com>
2025-08-31 23:39:40 +0530

    newcommit

--- server/server.js
+++ server/server.js
@@ -5,4 +5,7 @@ import 'dotenv/config';
 const app= express()
  app.use(cors())
  app.use(express.json())
- 
+
+ app.get('/',(req,res)=>res.send('Server is Live!'))
+
+ const PORT = process.env.PORT || 3000
